7f4b10c163a585cdc588509c27f2fce329d154c1
leejunhui <leejunhui2015@gmail.com>
2024-05-22 18:33:10 +0800

    refactor: 手动修改配置

--- src/internal/defaultConfiguration.ts
+++ src/internal/defaultConfiguration.ts
@@ -1,9 +1,9 @@
 import * as Types from './types';
 
 const DEFAULT_CONFIGURATION: Types.NetInfoConfiguration = {
-  reachabilityUrl: 'https://clients3.google.com/generate_204',
+  reachabilityUrl: 'https://app-prepub.bookln.cn/',
   reachabilityTest: (response: Response): Promise<boolean> =>
-    Promise.resolve(response.status === 204),
+    Promise.resolve(response.status === 200),
   reachabilityShortTimeout: 5 * 1000, // 5s
   reachabilityLongTimeout: 60 * 1000, // 60s
   reachabilityRequestTimeout: 15 * 1000, // 15s
